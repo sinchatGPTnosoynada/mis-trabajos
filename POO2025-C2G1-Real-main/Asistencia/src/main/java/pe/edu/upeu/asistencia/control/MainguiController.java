f59165ced18c9259a7bba438a2897ab9a0272846
sinchatGPTnosoynada <apazafrancisco20@gmail.com>
2025-09-12 14:36:24 -0500

    sesion7

--- POO2025-C2G1-Real-main/Asistencia/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
+++ POO2025-C2G1-Real-main/Asistencia/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
@@ -6,6 +6,7 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,24 +24,79 @@ public class MainguiController {
     @FXML
     private TabPane tabPane;
     @FXML
-    private Menu menu1;
+    private Menu menu1, menu2 = new Menu("Cambiar Estilo");
     @FXML
     private MenuItem menuItem1, menuItem2, menuItemC;
+
+    private ComboBox<String> comboBoxEstilo = new ComboBox<>();
+
+    private CustomMenuItem customMenuEstilo = new CustomMenuItem(comboBoxEstilo);
+
+
 
     @Autowired
     protected ApplicationContext context;
 
     @FXML
     public void initialize() {
+
+        comboBoxEstilo.getItems().addAll("Estilo azul", "Estilo ocsuro", "Estilo verde", "Estilo rosado");
+
+        comboBoxEstilo.setOnAction(actionEvent -> cambiarEstilo());
+
+        customMenuEstilo.setHideOnClick(false);
+
+        menu2.getItems().add(customMenuEstilo);
+
+        menuBar.getMenus().add(menu2);
+
         MenuListener mL = new MenuListener();
         MenuItemListener mIL = new MenuItemListener();
         menuItem1.setOnAction(mIL::handle);
         menuItem2.setOnAction(mIL::handle);
         menuItemC.setOnAction(mIL::handle);
     }
+
+    public void cambiarEstilo(){
+
+        String estilo = comboBoxEstilo.getSelectionModel().getSelectedItem();
+
+        Scene scene = bp.getScene();
+
+        scene.getStylesheets().clear();
+
+        switch (estilo){
+
+            case "Estilo ocsuro":
+
+                scene.getStylesheets().add(getClass().getResource("/css/estilo-oscuro.css").toExternalForm());
+                break;
+
+            case "Estilo azul":
+
+                scene.getStylesheets().add(getClass().getResource("/css/estilo-azul.css").toExternalForm());
+                break;
+
+            case "Estilo verde":
+
+                scene.getStylesheets().add(getClass().getResource("/css/estilo-verde.css").toExternalForm());
+                break;
+
+            case "Estilo rosado":
+
+                scene.getStylesheets().add(getClass().getResource("/css/estilo-rosado.css").toExternalForm());
+                break;
+
+            default:
+
+                break;
+        }
+
+    }
+
     class MenuItemListener{
         Map<String, String[]> menuConfig=Map.of(
-               "menuItem1", new String[]{"/fxml/main_asistencia.fxml","Gestion Asistencia","T"},
+                "menuItem1", new String[]{"/fxml/main_asistencia.fxml","Gestion Asistencia","T"},
                 "menuItem2", new String[]{"/fxml/main_participante.fxml","Gestion Participantes","T"},
                 "menuItemC", new String[]{"/fxml/login.fxml","Salir","C"}
         );
