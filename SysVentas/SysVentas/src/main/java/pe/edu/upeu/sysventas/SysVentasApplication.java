83da1d00937ee521a137d882fc29c7404bf93b4f
sinchatGPTnosoynada <apazafrancisco20@gmail.com>
2025-10-06 11:34:34 -0500

    proyecto de ventas

---
+++ SysVentas/SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.sysventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysVentasApplication.class, args);
+	}
+
+}
